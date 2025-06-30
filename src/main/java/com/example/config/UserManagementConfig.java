d380ca269ec7fd12da9601e7399c2651b5adc62b
sundalei <sundalei1988@gmail.com>
2025-06-30 14:51:57 +0800

    Code clean

--- src/main/java/com/example/config/UserManagementConfig.java
+++ src/main/java/com/example/config/UserManagementConfig.java
@@ -2,11 +2,9 @@ package com.example.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
 import javax.sql.DataSource;
