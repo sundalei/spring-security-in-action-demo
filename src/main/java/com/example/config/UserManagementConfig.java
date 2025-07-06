9964738df85b96498db3ea3eb6f894da7ec36d9e
sundalei <sundalei1988@gmail.com>
2025-07-06 23:13:47 +0800

    Switch user management to LDAP and update configuration

--- src/main/java/com/example/config/UserManagementConfig.java
+++ src/main/java/com/example/config/UserManagementConfig.java
@@ -2,27 +2,30 @@ package com.example.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.userdetails.User;
+import org.springframework.ldap.core.ContextSource;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.ldap.DefaultLdapUsernameToDnMapper;
+import org.springframework.security.ldap.userdetails.LdapUserDetailsManager;
 
 @Configuration
 public class UserManagementConfig {
 
+    private final ContextSource contextSource;
+
+    public UserManagementConfig(ContextSource contextSource) {
+        this.contextSource = contextSource;
+    }
+
     @Bean
     public UserDetailsService userDetailsService() {
 
-        var userDetailsService = new InMemoryUserDetailsManager();
+        var ldapUserDetailsManager = new LdapUserDetailsManager(contextSource);
+        ldapUserDetailsManager.setGroupSearchBase("ou=groups");
+        ldapUserDetailsManager.setUsernameMapper(new DefaultLdapUsernameToDnMapper("ou=groups", "uid"));
 
-        var user = User.withUsername("john")
-                .password("12345")
-                .authorities("read")
-                .build();
-
-        userDetailsService.createUser(user);
-        return userDetailsService;
+        return ldapUserDetailsManager;
     }
 
     @Bean
