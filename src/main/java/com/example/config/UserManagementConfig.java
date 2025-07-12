8abffecd1612b78585418e2c9883431b21cfd021
sundalei <sundalei2011@163.com>
2025-07-12 23:07:01 +0800

    Refactor code formatting and indentation for consistency
    (cherry picked from commit b3f145dd3bc249541ab35ffbee709074cbb1867c)

--- src/main/java/com/example/config/UserManagementConfig.java
+++ src/main/java/com/example/config/UserManagementConfig.java
@@ -4,29 +4,30 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class UserManagementConfig {
 
-  @Bean
-  public UserDetailsService userDetailsService() {
+    @Bean
+    public UserDetailsService userDetailsService() {
 
-    var userDetailsService = new InMemoryUserDetailsManager();
+        var userDetailsService = new InMemoryUserDetailsManager();
 
-    var user = User.withUsername("john")
-        .password("12345")
-        .authorities("read")
-        .passwordEncoder(s -> new Sha512PasswordEncoder().encode(s))
-        .build();
+        var user = User.withUsername("john")
+                .password("12345")
+                .authorities("read")
+                .build();
 
-    userDetailsService.createUser(user);
-    return userDetailsService;
-  }
+        userDetailsService.createUser(user);
+        return userDetailsService;
+    }
 
-  @Bean
-  public PasswordEncoder passwordEncoder() {
-    return new Sha512PasswordEncoder();
-  }
+    @Bean
+    @SuppressWarnings("deprecation")
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
 }
