a78927ce33488a011f91bca2da463565d25bbd80
sundalei <sundalei2011@163.com>
2025-07-13 13:06:32 +0800

    Remove unused Checkstyle XPath suppressions and update main method documentation
    (cherry picked from commit 8c598edd1fa892f004f6b188602ff25229984754)

--- src/main/java/com/example/SpringSecurityInActionDemoApplication.java
+++ src/main/java/com/example/SpringSecurityInActionDemoApplication.java
@@ -6,7 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringSecurityInActionDemoApplication {
 
-  public static void main(String[] args) {
+  /**
+   * Entry point
+   */
+  public static void main(final String[] args) {
     SpringApplication.run(SpringSecurityInActionDemoApplication.class, args);
   }
 }
